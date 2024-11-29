71eb9c5a79403e2be6bfc3f4e5f8c1a9cd6f3c5c
Evelyn Bordeau <ebordeau@gmail.com>
2024-11-29 18:40:17 -0500

    Support subdomains

--- app/.well-known/atproto-did/route.ts
+++ app/.well-known/atproto-did/route.ts
@@ -1,19 +1,20 @@
-export async function GET(request: Request) {
-    const url = new URL(request.url);
-    console.log(request.url);
+import { NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+    const url = request.nextUrl;
+    const hostname = request.headers.get('host')!;
+    const subdomain = hostname.split('.')[0].replace('localhost:3000', '');
+
+    console.log(`URL: ${url}`);
+    console.log(`Hostname: ${hostname}`);
+    console.log(`Subdomain: ${subdomain}`);
 
     let did;
-    if (url?.hostname) {
-        const parts = url.hostname.split('.');
-        if (parts.length === 3) {
-            // subdomain for individual user account
-            const username = parts[0];
-            // TODO: find did for user
-            did = 'did:plc:username';
-        } else if (parts.length === 2) {
-            // root domain for main account
-            did = 'did:plc:iqa6bfednevr3dztjkz5nd2m';
-        }
+    if (subdomain) {
+        did = 'did:plc:username';
+    } else {
+        // root domain for main account
+        did = 'did:plc:iqa6bfednevr3dztjkz5nd2m';
     }
 
     if (did) {
