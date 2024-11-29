cc93c050c4b27a8783123b95b4babe5061de13eb
Evelyn Bordeau <ebordeau@gmail.com>
2024-11-29 18:55:20 -0500

    Fix subdomain handling

--- app/.well-known/atproto-did/route.ts
+++ app/.well-known/atproto-did/route.ts
@@ -1,32 +1,25 @@
 import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
-    const url = request.nextUrl;
     const hostname = request.headers.get('host')!;
-    const subdomain = hostname.split('.')[0].replace('localhost:3000', '');
+    const segments = hostname.split('.');
+    const subdomain = segments.length === 3 ? segments[0] : '';
 
-    console.log(`URL: ${url}`);
     console.log(`Hostname: ${hostname}`);
     console.log(`Subdomain: ${subdomain}`);
 
     let did;
     if (subdomain) {
-        did = 'did:plc:username';
+        did = `did:plc:${subdomain}`;
     } else {
         // root domain for main account
         did = 'did:plc:iqa6bfednevr3dztjkz5nd2m';
     }
 
-    if (did) {
-        return new Response(did, { 
-            status: 200, 
-            headers: {
-                'Content-Type': 'text/plain',
-            }
-        });
-    }
-
-    return new Response('Bad Request', { 
-        status: 400, 
+    return new Response(did, { 
+        status: 200, 
+        headers: {
+            'Content-Type': 'text/plain',
+        }
     });
 }
