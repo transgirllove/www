a43c7b049337e81498bf303efc00f01c369f0bc0
Evelyn Bordeau <ebordeau@gmail.com>
2024-12-01 02:05:03 -0500

    Remove auth, leave home page blank

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { Amplify } from "aws-amplify";
 import "./app.css";
-import { Authenticator } from "@aws-amplify/ui-react";
+// import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import outputs from "@/amplify_outputs.json";
 
@@ -17,9 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>      
-        <Authenticator>
+        {/* <Authenticator> */}
           {children}
-        </Authenticator>
+        {/* </Authenticator> */}
       </body>
     </html>
   );
